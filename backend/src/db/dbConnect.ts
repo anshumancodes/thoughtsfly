0bf41b92911c7133e26f2482bd5b0dff10fbb580
anshumancodes <anshumanprof01@gmail.com>
2024-11-18 06:46:34 +0530

    add dbConnect : mongodb connection function

---
+++ backend/src/db/dbConnect.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const connectDb=async () => {
+
+    try {
+      
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_CONNECTION_URI}/${process.env.DB_NAME}`);
+
+        // console.log(`\n mongodb sucessfully connected! DB Host = ${connectionInstance.connection.host}`)
+       
+    } catch (error) {
+        console.log('Error connecting to MongoDB:[at connectDb]', error);
+        process.exit(1);
+       
+        
+    }
+    
+};
+
+export { connectDb };
