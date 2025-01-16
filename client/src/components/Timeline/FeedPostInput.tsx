70defe95d0fd69bf1e680c154a6495fdd9c97a24
anshumancodes <anshumanprof01@gmail.com>
2025-01-17 02:05:46 +0530

    refactor: update function and make CSS adjustments in FeedPostInput-m- Modified HandleInput to HandlePostInputResizing, improved textarea resizing and ux . - Updated CSS to enhance [add coniditional scrollbar on exceeding height of 400px to text area]

--- client/src/components/Timeline/FeedPostInput.tsx
+++ client/src/components/Timeline/FeedPostInput.tsx
@@ -4,24 +4,39 @@ import { useState, useRef } from "react";
 const FeedPostInput = () => {
   const [height, setHeight] = useState(140); // Initial height
   const textareaRef = useRef(null);
+  
+ 
 
-  const handleInput = (e) => {
-    setHeight("auto"); // Reset height before recalculating
-    setHeight(e.target.scrollHeight); // Set to scrollHeight for dynamic resize
-  };
+  const HandlePostInputResizing=()=>{
+    const textarea = textareaRef.current;
+    if (textarea && textarea.scrollHeight >= 141) {
+      textarea.style.height="auto";
+      textarea.style.height = `${textarea.scrollHeight}px`;
+      // setHeight(textarea.scrollHeight);
+    }
+    else{
+      setHeight(140);
+    }
+  }
 
+  
+  
+  
   return (
     <div>
-      {/* Input Section */}
-      <div className="mb-4 border border-gray-500 h-auto">
-        <textarea
-          ref={textareaRef}
-          placeholder="What's happening?"
-          className="w-full p-3 outline-none bg-inherit resize-none overflow-hidden"
-          style={{ height: `${height}px` }}
-          onInput={handleInput}
-        />
+     
+      <div className={`mb-4 border border-gray-500 h-auto`}>
+      <textarea
+      ref={textareaRef}
+      placeholder="What's happening?"
+      className={` w-full p-3 outline-none bg-inherit resize-none  ${height < 400 ? 'h-auto' : 'overflow-auto'} max-h-[400px]`}
+
+
+      style={{ height: `${height}px` }}
+      onInput={HandlePostInputResizing}
+    />
         <div className="w-full flex justify-center">
+          
           <div className="h-[1px] bg-gray-500 mb-1 w-full"></div>
         </div>
         <div className="flex w-full items-center justify-between pb-4 pr-4 pl-4 space-x-4">
