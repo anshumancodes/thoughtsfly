9a8062827d6eb51e4faf888deb01b011e9861fad
anshumancodes <anshumanprof01@gmail.com>
2025-02-05 03:03:25 +0530

    Add additional Recoil state atoms for UI and post interactions-m
    - Added activeTabState for tracking active tab.
    - Added CreatePostModalState to manage create post modal visibility.
    - Introduced ExploreTabNavigationState for Explore tab navigation.
    - Added UpVoteState and UpVoteCountState for handling upvote functionality.
    - Added EditProfileModalState for managing profile edit modal visibility.

--- client/src/context/Atoms.ts
+++ client/src/context/Atoms.ts
@@ -1,4 +1,5 @@
 import { atom } from "recoil";
+
 export const loginModalState = atom({
   key: "loginModalState",
   default: false,
@@ -12,4 +13,34 @@ export const LightOrDark = atom({
 export const ContentTabState=atom({
   key:"FeedContentSelector",
   default:"for-you"
+});
+
+export const activeTabState = atom({
+  key: "activeTabState", // Unique key for this atom
+  default: "Posts", // Default value for the active tab
+});
+
+export const CreatePostModalState = atom({
+  key: "CreatePostModalState",
+  default: false,
+});
+
+export const ExploreTabNavigationState=atom({
+  key:"ExploreTabNavigationState",
+  default:"For You"
+})
+
+export const UpVoteState=atom({
+  key:"UpVoteState",
+  default:false
+})
+
+export const UpVoteCountState=atom({
+  key:"UpVoteCountState",
+  default:0
+})
+
+export const EditProfileModalState=atom({
+  key:"EditProfileModalState",
+  default:false
 })
