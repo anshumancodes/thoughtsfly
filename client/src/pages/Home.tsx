a82679d4fb8e636b16ea2897da0a573a5adaaee4
anshumancodes <anshumanprof01@gmail.com>
2025-02-05 01:53:20 +0530

    refactor: enhance Home component with CreatePostModal integration and Feed

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import CreatePostModal from "../components/Timeline/CreatePostModal"
+import Feed from "../components/Timeline/Feed"
+import { CreatePostModalState } from "../context/Atoms"
+import { useRecoilValue } from "recoil"
 const Home = () => {
+  const CreatePostModalVisibility=useRecoilValue(CreatePostModalState)
   return (
-    <div>Home</div>
+    <main className="">
+     
+      <Feed  />  {/* Feed takes up the remaining space */}
+      {CreatePostModalVisibility && <CreatePostModal />}
+     
+    </main>
   )
 }
 
 export default Home
+
