8f64cacf797422eb9aec6668c419f916974a7918
anshumancodes <anshumanprof01@gmail.com>
2025-02-06 23:55:51 +0530

    feat: add Auth0 and Recoil state management
    Integrated Auth0Provider for authentication with domain and client ID configuration. Wrapped the app with RecoilRoot to enable state management using Recoil. Ensured compatibility by maintaining StrictMode and BrowserRouter setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,26 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { RecoilRoot } from "recoil";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+   
+    <RecoilRoot>
+    <Auth0Provider
+       domain="dev-2jw4zujxnfrfeb4x.us.auth0.com"
+       clientId="SYfKxCk7NXwGtSSMOWJKzZnh3mIvASfy"
+       authorizationParams={{
+         redirect_uri: window.location.origin
+       }}
+      >
     <BrowserRouter>
+
+      
       <App />
+      
     </BrowserRouter>
+    </Auth0Provider>
+    </RecoilRoot>
   </StrictMode>
 );
