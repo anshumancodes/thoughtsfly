512513b96c89f245d222d87b9a0deccf5ddf12d2
anshumancodes <anshumanprof01@gmail.com>
2025-02-28 23:14:40 +0530

    Refactor AppLayout for Responsive Design and Conditional Rendering

--- client/src/Layouts/AppLayout.tsx
+++ client/src/Layouts/AppLayout.tsx
@@ -1,19 +1,33 @@
-import SideNav from '../components/Nav/SideNav'
-import TrendingCard from '../components/Trending/TrendingCard'
-import { Outlet } from 'react-router-dom'
+import SideNav from '../components/Nav/SideNav';
+import TrendingCard from '../components/Trending/TrendingCard';
+import { Outlet } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { isSettingson } from '../context/Atoms';
+import MobileNav from '../components/Nav/MobileNav';
+
 const AppLayout = () => {
-    
+  const settingTabState = useRecoilValue(isSettingson);
+
   return (
-    <main className="mx-auto flex gap-1 max-w-6xl px-4">
-      <SideNav  /> 
-      <div className='w-[800px] h-[100vh] overflow-y-auto border-r border-gray-500 hide-scrollbar flex justify-center'>
-      <Outlet />
+    <main className="mx-auto flex flex-col md:flex-row  max-w-6xl md:px-4">
+    
+      <div className="hidden md:block">
+        <SideNav />
       </div>
-      <div>
-        <TrendingCard />
+     
+      <div className="">
+        <Outlet />
+      </div>
+     
+      <div className="md:hidden">
+        <MobileNav />
+      </div>
+     
+      <div className="hidden lg:block">
+        {settingTabState ? null : <TrendingCard />}
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default AppLayout
+export default AppLayout;
