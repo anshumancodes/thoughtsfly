8f453c96e0f4836fd5ed17ff98fa0c4ae9a4d083
anshumancodes <anshumanprof01@gmail.com>
2025-03-02 03:53:55 +0530

    feat(CreatePostModal): add theme support and responsive design improvements-m- Integrated theme support by utilizing the LightOrDark Recoil state to conditionally style the modal. - Refactored container and textarea styling for improved responsiveness (e.g., responsive widths, padding adjustments, and maxLength on textarea). - Removed auto-resizing logic and unused height state to simplify the component

--- client/src/components/Timeline/CreatePostModal.tsx
+++ client/src/components/Timeline/CreatePostModal.tsx
@@ -1,26 +1,26 @@
 import { X } from "lucide-react";
-import { useState, useRef } from "react";
+import {useRef } from "react";
 import { CreatePostModalState } from "../../context/Atoms";
-import { useSetRecoilState } from "recoil";
+import { useSetRecoilState,useRecoilValue } from "recoil";
+
+import { LightOrDark } from "../../context/Atoms";
 const CreatePostModal = () => {
+  const theme = useRecoilValue(LightOrDark);
+  
   const setCreatePostModalState = useSetRecoilState(CreatePostModalState);
-  const [height, setHeight] = useState(180); // Initial height
+
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
 
-  const HandlePostInputResizing = () => {
-    const textarea = textareaRef.current;
-    if (textarea && textarea.scrollHeight >= 141) {
-      textarea.style.height = "auto";
-      textarea.style.height = `${textarea.scrollHeight}px`;
-    } else {
-      setHeight(140);
-    }
-  };
+ 
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 z-50 flex justify-center mt-20">
-      <div className="px-4 py-4 w-[600px] h-[360px] rounded-[32px] bg-[#1e1e1e] shadow-md mr-32 mt-20">
-        <div className="flex justify-between items-center">
-          <button onClick={()=>setCreatePostModalState(false)}>
+    <div className="fixed top-0 left-0 w-screen h-full bg-[#1e1e1e] bg-opacity-50 z-40 flex justify-center ">
+      <div  className={`${
+        theme === "light"
+          ? "bg-[#FFFFFA] text-grey-900"
+          : "bg-[#1e1e1e]  text-[#FFFFFA]"
+      }px-2 md:px-4 md:py-4 w-[99%]  md:w-[600px] h-[360px] rounded-[32px]  shadow-md md:mr-32 mt-20 z-50`}>
+        <div className="flex justify-between items-center p-2 md:p-0">
+          <button onClick={()=>setCreatePostModalState(false)} >
             <X />
           </button>
           <button className="text-[#e14f20] font-bold px-2 py-1">Draft</button>
@@ -36,15 +36,13 @@ const CreatePostModal = () => {
           <textarea
             ref={textareaRef}
             placeholder="What's happening?"
-            className={` w-full p-3 outline-none bg-inherit resize-none  ${
-              height < 400 ? "h-auto" : "overflow-auto"
-            } max-h-[400px]`}
-            style={{ height: `${height}px` }}
-            onInput={HandlePostInputResizing}
+            maxLength={1000}
+            className={` w-full p-3 outline-none bg-inherit resize-none min-h-[180px] overflow-y-auto hide-scrollbar [&:valid]:caret-white [&:invalid]:caret-red-600`}
+            
           />
         </div>
          <div className="h-[1px] bg-gray-500 w-full"></div>
-        <div className="flex justify-between items-center mt-4 ">
+        <div className="flex justify-between items-center mt-4 px-2 md:px-1">
            <div className="flex items-center gap-4">
            <img src="/img-box-svgrepo-com.svg" alt="" width={24} height={24} />
            <img src="/gif-svgrepo-com.svg" alt="" width={24} height={24} />
