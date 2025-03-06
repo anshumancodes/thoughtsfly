39757d37a93023e0c0ec604009f9b415b826b624
anshumancodes <anshumanprof01@gmail.com>
2025-03-06 23:56:53 +0530

    Remove fixed positioning from LogoutModal container

--- client/src/components/auth/LogoutModal.tsx
+++ client/src/components/auth/LogoutModal.tsx
@@ -4,8 +4,8 @@ const LogoutModal = () => {
     const {logout}=useAuth0()
   return (
     <div>
-        <div className=" fixed w-[160px] rounded-md">
-            <button className="text-black border-gray-600 border  px-14 py-2 rounded-[32px] font-bold" onClick={()=>logout()}>logout</button>
+        <div className="  w-[160px] rounded-md">
+            <button className=" border-gray-600 border  px-14 py-2 rounded-[32px] font-bold" onClick={()=>logout()}>logout</button>
 
       
       </div>
