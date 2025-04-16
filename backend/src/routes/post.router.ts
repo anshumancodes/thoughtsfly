697bf3929ad62bd1e53f5a61c8c01c12a5576dc5
anshumancodes <anshumanprof01@gmail.com>
2025-04-17 03:08:53 +0530

    Enhance post routes with user info extraction and new endpoints-m- Integrated ExtractUserInfo middleware to extract user details for relevant routes - Added /fetchuserPosts and /fetchFeed routes for fetching user posts and all posts respectively - Updated fetchPostbyid route for consistent naming convention - Refined authorization and middleware checks for improved request handling

--- backend/src/routes/post.router.ts
+++ backend/src/routes/post.router.ts
@@ -1,11 +1,13 @@
 import Router from "express";
 const router = Router();
-import { createPost,deletePost,fetchPostbyid } from "../controllers/post.controller.js";
-import { jwtCheck } from "../middlewares/Auth.middleware.js";
+import { createPost,deletePost,fetchPostbyid,fetchUserallposts,fetchFeed } from "../controllers/post.controller.js";
+import { jwtCheck,ExtractUserInfo } from "../middlewares/Auth.middleware.js";
 
 
-router.route("/create").post(jwtCheck,createPost);
+router.route("/create").post(jwtCheck,ExtractUserInfo,createPost);
 router.route("/delete/:id").delete(jwtCheck,deletePost);
-router.route("/:postid").get(jwtCheck,fetchPostbyid)
+router.route("/fetch/:postid").get(jwtCheck,fetchPostbyid);
+router.route("/fetchuserPosts").get(jwtCheck,ExtractUserInfo,fetchUserallposts);
+router.route("/fetchFeed").get(jwtCheck,fetchFeed)
 
 export default router;
