e7ca66da5d771802410cc9976b31f3ffc0bf6da0
anshumancodes <anshumanprof01@gmail.com>
2025-04-19 02:44:12 +0530

    Set up API routing and add root endpoint-m
    - Configured CORS to allow all origins for development flexibility
    - Added a standard GET route at '/' for API health check and route discovery
    - Integrated user and post route modules under /api/v1/user and /api/v1/post

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors(
-    {
+    {   origin:"*",
         credentials: true ,
         methods: ['GET', 'POST', 'OPTIONS'], // Allowed methods
         allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
@@ -18,6 +18,31 @@ app.use(cookieparser());
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({ extended: true ,limit: '120kb' }));
 app.use(bodyparser.json());
+
+// standard GET route
+app.get('/', (req, res) => {
+    res.status(200).json({
+      message: 'Hello from thoughtsfly api!',
+      version: "1.0.0",
+      routes: [
+        "/api/v1/user",
+        "/api/v1/thought",
+        "/api/v1/comment",
+        "/api/v1/like",
+        "/api/v1/bookmark",
+      ]
+    });
+  });
+
+
+// user routes
+import userRouter from "./routes/user.router.js";
+app.use("/api/v1/user", userRouter);
+// post routes
+import postRouter from "./routes/post.router.js"
+app.use("/api/v1/post", postRouter);
+// 
+
 
 
 
