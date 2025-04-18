c43e7b99816b0f9a895d16ab21e858280353baec
anshumancodes <anshumanprof01@gmail.com>
2025-04-19 05:16:03 +0530

    Fetch post dynamically from backend and display in PostViewWithComments-m- Added API integration to fetch a specific post by ID using  and  from . - Introduced authentication with  to fetch the post with a token using . - Updated  to conditionally render the post fetched from the API. - Refined UI with loading state when fetching post data. - Enhanced  component to accept dynamic  prop representing the fetched post.

--- client/src/components/Comments/PostViewWithComments.tsx
+++ client/src/components/Comments/PostViewWithComments.tsx
@@ -1,8 +1,41 @@
 import Post from "../Timeline/Post";
 import { ArrowLeft } from "lucide-react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ReplyInput from "./ReplyInput";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+import { PostData } from "../Timeline/Post";
 const PostViewWithComments = () => {
+  const { getAccessTokenSilently, isLoading, isAuthenticated } = useAuth0();
+  const { postId } = useParams();
+  const [post, setPost] = useState<PostData | null>(null);
+
+  const fetchPost = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await axios.get(
+        `http://localhost:8001/api/v1/post/fetch/${postId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("userposts", response);
+      setPost(response.data.data.post);
+    } catch (error) {
+      console.log("unable to fetch this post", error);
+    }
+  };
+
+  useEffect(() => {
+    if (!isLoading && isAuthenticated) {
+      fetchPost();
+    }
+  }, [postId]);
+
   return (
     <div>
       <div className="flex gap-12 ml-4 items-center">
@@ -13,10 +46,14 @@ const PostViewWithComments = () => {
         <p className="text-xl font-bold">Post</p>
       </div>
       <div className="mt-5">
-        <Post />  {/* Post component with content grab the data from the backend with reference to the post id, this is just template reference now */}
+        {post ? (
+          <Post data={post} />
+        ) : (
+          <p className="text-gray-500 text-sm px-4">Loading post...</p>
+        )}
       </div>
       <div className="border-b border-gray-500 w-[605px]">
-        <ReplyInput/>
+        <ReplyInput />
       </div>
       {/* comments trailing here  will be added later */}
     </div>
