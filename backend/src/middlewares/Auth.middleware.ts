586ee8d672968dc28e50ab8e6994586fdef597bb
anshumancodes <anshumanprof01@gmail.com>
2025-09-18 03:29:53 +0530

    fix(auth): specify RS256 algorithm in jwtCheck middleware

--- backend/src/middlewares/Auth.middleware.ts
+++ backend/src/middlewares/Auth.middleware.ts
@@ -5,6 +5,7 @@ import User from "../models/user.model.js";
 const jwtCheck = auth({
   audience: 'thoughtsfly auth users',
   issuerBaseURL: 'https://dev-2jw4zujxnfrfeb4x.us.auth0.com/',
+  tokenSigningAlg: 'RS256',
   
 });
 
